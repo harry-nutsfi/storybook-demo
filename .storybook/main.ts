d39c7cbb9080192764a6e670ca64c9aa5ede4188
harry-nutsfi <harry@nuts.finance>
2023-07-26 12:16:14 +0700

    build: init project

---
+++ .storybook/main.ts
@@ -0,0 +1,29 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  webpackFinal: async (config) => {
+    (config.resolve as any).alias = {
+      ["@"]: [path.resolve(__dirname, "../src")],
+      public: path.resolve(__dirname, "../public"),
+    };
+
+    return config;
+  },
+  staticDirs: ["../public"],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
