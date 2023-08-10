e05573f82cdc032c0ff22aad6e65adbe90b9c9f6
harry-nutsfi <harry@nuts.finance>
2023-08-11 00:45:01 +0700

    test: test playwright CI

--- tests/e2e/fixtures.ts
+++ tests/e2e/fixtures.ts
@@ -19,6 +19,10 @@ export const test = base.extend<{
       "--remote-debugging-port=9222",
     ];
 
+    if (process.env.CI) {
+      browserArgs.push("--disable-gpu");
+    }
+
     if (process.env.E2E_HEADLESS_MODE) {
       browserArgs.push("--headless=new");
     }
@@ -31,8 +35,7 @@ export const test = base.extend<{
     await context.pages()[0].waitForTimeout(3000);
     // setup metamask
     await initialSetup(chromium, {
-      secretWordsOrPrivateKey:
-        "test test test test test test test test test test test junk",
+      secretWordsOrPrivateKey: process.env.E2E_METAMASK_SEED_PHRASE,
       network: "goerli",
       password: "e2eTesting",
       enableAdvancedSettings: true,
