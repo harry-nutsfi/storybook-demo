09cf5770ec08c354874434ce3984bc8762e4bcfe
harry-nutsfi <harry@nuts.finance>
2024-06-06 20:04:46 +0700

    fix: check snapshot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <span className="target">Harry</span>
-      OK
+      OK OK again
     </main>
   )
 }
